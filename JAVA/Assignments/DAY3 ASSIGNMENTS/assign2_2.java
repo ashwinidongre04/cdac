a2aeb8d7259b2270dffd86b75df40b1c02bbd38c
ashwinidongre04 <ashwini42002dongre@gmail.com>
2024-09-05 22:39:21 +0530

    Add files via upload

---
+++ JAVA/Assignments/DAY3 ASSIGNMENTS/assign2_2.java
@@ -0,0 +1,7 @@
+public class assign2_2 {
+    public static void main(String[] args)
+    {
+        System.out.println(Short.MIN_VALUE );
+        System.out.println(Short.MAX_VALUE );
+}
+}
---
+++ JAVA/Assignments/DAY3 ASSIGNMENTS/assign_2_7.java
@@ -0,0 +1,9 @@
+public class assign_2_7 {
+
+        public static void main(String[] args)
+        {
+         String str = "a1b2c3";
+         Short s =  Short.parseShort(str);
+         System.out.println(s);
+    }
+}
