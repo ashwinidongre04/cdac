e27f99bc15d44d2c0083fb202b9e99c5834a123e
ashwinidongre04 <ashwini42002dongre@gmail.com>
2024-09-06 11:48:13 +0530

    Add files via upload

---
+++ JAVA/Assignments/DAY3 ASSIGNMENTS/assign4.java
@@ -0,0 +1,65 @@
+public class assign4
+{
+public static void main(String[] args) {
+    System.out.println( Double.BYTES);
+}
+}
+
+/*2)
+ public static void main(String[] args) {
+        System.out.println( Double.MIN_VALUE);
+        System.out.println( Double.MAX_VALUE);
+    }
+
+    3)
+    public static void main(String[] args) {
+        double number = 45.67;
+        String strNumber = Double.toString(number);
+        System.out.println("Double to String: " + strNumber);
+    }
+
+    4)
+       public static void main(String[] args) {
+        String strNumber = "123.45";
+        double number = Double.parseDouble(strNumber);
+        System.out.println("String to double: " + number);
+    }
+        5)
+         public static void main(String[] args) {
+       
+            String strNumber = "Ab12Cd3";
+            double number = Double.parseDouble(strNumber);
+       
+            System.out.println(number);
+}
+        6)
+        public static void main(String[] args) {
+        double number = 789.56;
+        Double wrapperNumber = Double.valueOf(number);
+        System.out.println( wrapperNumber);
+}
+        7)
+         public static void main(String[] args) {
+        String strNumber = "456.78";
+        Double wrapperNumber = Double.valueOf(strNumber);
+        System.out.println("String to Double wrapper: " + wrapperNumber);
+    }
+        8)
+        public static void main(String[] args) {
+        double num1 = 112.3;
+        double num2 = 984.5;
+        double sum = Double.sum(num1, num2);
+        System.out.println("Sum of two doubles: " + sum);
+    }
+        9)
+         public static void main(String[] args) {
+        double num1 = 112.2;
+        double num2 = 556.6;
+        double min = Double.min(num1, num2);
+        double max = Double.max(num1, num2);
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+    }
+        
+    
+ */
